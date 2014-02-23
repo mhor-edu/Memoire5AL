94d6fc4b5d49637c8f8e98f865df2fa1996140d1
Cédric TESNIERE <tesniere.cedric@gmail.com>
2014-02-23 21:12:49 +0100

    up

--- generate_of_report.sh
+++ generate_of_report.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "suppresion de l'ancienne version"
+
+rm memoire.pdf
+
 echo "Generate of report"
+
 pandoc -N --template=templates/mytemplate.tex --variable mainfont=Georgia --variable sansfont=Arial --variable monofont="Bitstream Vera Sans Mono" --variable fontsize=12pt -V geometry:"top=3cm, bottom=3cm, left=3cm, right=3cm" -s --highlight-style haddock Chapters/1* Chapters/2* Chapters/3* Chapters/Outils/* Chapters/4* Chapters/Methods/* Chapters/5* Chapters/6* Chapters/7* --latex-engine=pdflatex --toc -o memoire.pdf
+
 echo "Done Generate of report"
 
 echo "Fusion"
@@ -9,3 +15,4 @@ gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=output.pdf memoire.pdf plagi
 echo "Done Fusion"
 
 open output.pdf
+
