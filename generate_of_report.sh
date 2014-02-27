78bde8d9d1fe5ad39853df555a90898b17ea1f06
Cédric TESNIERE <tesniere.cedric@gmail.com>
2014-02-27 22:03:07 +0100

    Fix : toc depth

--- generate_of_report.sh
+++ generate_of_report.sh
@@ -7,7 +7,7 @@ rm output.pdf
 
 echo "Generate of report"
 
-pandoc -N --template=templates/mytemplate.tex --variable mainfont=Georgia --variable sansfont=Arial --variable monofont="Bitstream Vera Sans Mono" --variable fontsize=12pt -V geometry:"top=3cm, bottom=3cm, left=3cm, right=3cm" -s --highlight-style haddock summary/* Chapters/1* Chapters/2* Chapters/3* Chapters/Outils/* Chapters/4* Chapters/Methods/* Chapters/5* Chapters/6* Chapters/7* --latex-engine=pdflatex --toc -o output.pdf
+pandoc -N --template=templates/mytemplate.tex --variable mainfont=Georgia --variable sansfont=Arial --variable monofont="Bitstream Vera Sans Mono" --variable toc-depth=6 --variable fontsize=12pt -V geometry:"top=3cm, bottom=3cm, left=3cm, right=3cm" -s --highlight-style haddock summary/* Chapters/1* Chapters/2* Chapters/3* Chapters/Outils/* Chapters/4* Chapters/Methods/* Chapters/5* Chapters/6* Chapters/7* --latex-engine=pdflatex --toc -o output.pdf
 
 echo "Done Generate of report"
 
